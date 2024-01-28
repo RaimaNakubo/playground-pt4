2351bb3e63c219fdff7e8b56df10cf4e34181dd2
Nikita Belenkov <bna.gorzar@gmail.com>
2024-01-28 10:00:05 +0300

    A tour of Go n23: type switches and stringers

--- methods-and-interfaces.go
+++ methods-and-interfaces.go
@@ -4,40 +4,50 @@ import (
 	"fmt"
 )
 
+// type Person describes a person by it's name and age
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
-	//empty interfaces
-	var i interface{} //i is the instance of an empty interface i(nil,nil)
-	describe(i)
-
-	i = 42 //i(42,int)
-	describe(i)
-
-	i = "hello" //i("hello",string)
-	describe(i)
-
-	//fmt.Print() <-- takes any number of arguments of interface{} type
+	//type switches
+	do(21)      //testing type int
+	do("hello") //testing type string
+	do(true)    //testing type bool
 	fmt.Println()
 
-	//interface's type assertions
-	var j interface{} = "hello" //j("hello", string)
-
-	s := j.(string) //s("hello",string)
-	fmt.Println(s)
-
-	s, ok := j.(string) //s == hello, ok == true
-	fmt.Println(s, ok)
-
-	f, ok := j.(float64) //f == 0.0, ok == false
-	fmt.Println(f, ok)
-
-	/*
-		f = j.(float64) //panic "interface conversion: interface {} is string, not float64"
-						//this happened bc statement j.(float64) asserts that value j holds float64 type, but actually it holds string type; do not test like this
-		fmt.Println(f)
-	*/
+	//stringers
+	person1 := Person{"Roman Muchalov", 25}
+	person2 := Person{"Nikolay Dolzhansky", 44}
+	fmt.Println(person1, person2)
+	//fmt.Printf("%v, %v", person1, person2)
 }
 
-// function describe() prints value and type of interface{} instance
-func describe(i interface{}) {
-	fmt.Printf("Interface instance value: %v, type: %T\n", i, i)
+// function do() runs a type swtch test for interface{} and prints statements for int, string and unknown types
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+
+	}
 }
+
+// method String() recieves value Person and returns a formatted string to print;
+// this method implements Stringer interface from print.go, bc it has String() method name and type of returning value is string
+// if a Stringer is present it redefines how strings are printed inside current file
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+/*
+ Stringer is implemented by any value that has a String method,           \fmt\print.go
+ which defines the “native” format for that value.
+ The String method is used to print values passed as an operand
+ to any format that accepts a string or to an unformatted printer
+ such as Print.
+*/
