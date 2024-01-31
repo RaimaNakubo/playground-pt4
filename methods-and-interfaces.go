dce9736c74ded68b3bd51689bf4d1901fd2abdb3
Nikita Belenkov <bna.gorzar@gmail.com>
2024-01-31 14:16:52 +0300

    A tour of Go n31: Exercise: Images

--- methods-and-interfaces.go
+++ methods-and-interfaces.go
@@ -1,14 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
 )
+
+// MyImage is a custom type for image interface;
+// this type will implement Image interface;
+// resulting image will be 255x255 rectangle with color in RGBA model
+type MyImage struct {
+	//here migh've been some properties for an image
+	//like height, width and base colours in RGBA
+}
 
 func main() {
 	//images
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100)) //m is instance of RGBA image with boundaries, starting point inside boundaries is 0,0, endpoint is 100,100
-	fmt.Println(m.Bounds())                        //printing image's boundaries
-	fmt.Println(m.At(0, 0).RGBA())                 //getting color of point of an image at 0,0 coordinates as RGBA value
+	m := MyImage{}
+	pic.ShowImage(&m)
 
+}
+
+// method ColorModel() returns RGBA color model for MyImage type of instance
+func (MyImage) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+// method Bounds() sets image's boundaries for MyImage type of instance
+func (MyImage) Bounds() image.Rectangle {
+
+	return image.Rect(0, 0, 255, 255)
+}
+
+// method At() returns the color of an Image point in instance i as RGBA
+func (i *MyImage) At(x, y int) color.Color {
+	v := uint8((x^y)*((x+y)/2) + (x * y))
+	return color.RGBA{v, v, 255, 255}
 }
